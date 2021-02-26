1d801edd9042ea1bdec287272e59bb86fe8c9914
Zexal0807 <gallinar00@gmail.com>
2021-02-26 09:44:47 +0100

    Fix indent

--- Test.js
+++ Test.js
@@ -1,29 +1,27 @@
-module.exports = class Test{
+module.exports = class Test {
 	name;
 	url;
 	method;
 	headers;
 	body;
 	aspectedResponse;
-	
-	costruction(obj){
+
+	costruction(obj) {
 		this.name = name || "";
 		this.url = obj.url || "/";
 		this.method = obj.method || "GET";
 		this.headers = obj.headers || [];
-		if(this.headers['Content-Type'] == undefined){
+		if (this.headers['Content-Type'] == undefined) {
 			this.headers['Content-Type'] = "application/json";
 		}
 		this.body = obj.body || {}
-		this.aspectedResponse = obj.response || {status:true};
+		this.aspectedResponse = obj.response || { status: true };
 	}
-	
-	test(request){
-		
+
+	test(request) {
 		it(this.name, async done => {
 			var method = this.method;
-			const response = await request
-				[method](this.url)
+			const response = await request[method](this.url)
 				.set(this.headers)
 				.send(this.body);
 
